da9caed0928667afe6d93ff4e357da93b66644d1
JJue888 <joshuajue888@gmail.com>
2025-12-31 00:11:53 -0800

    navbar

--- app/ui/navigationComponents/nav-links.tsx
+++ app/ui/navigationComponents/nav-links.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import {usePathname} from "next/navigation";
+import Link from "next/link";
+import classes from "./nav-links.module.css"
 
 const links = [
     {name: 'Home', link: '/dashboard'},
-    {name: 'PNMS', link: '/pnms'},
+    {name: 'PNMS', link: '/dashboard/pnms'},
+    {name: 'Add PNM', link: '/dashboard/pnms/create'}
 ]
 
 export default function NavLinks() {
@@ -12,7 +15,13 @@ export default function NavLinks() {
 
     return (
         <>
-
+            {links.map((link) => {
+                return (
+                    <Link key={link.link} href={link.link} className={classes.navText}>
+                        <p>{link.name}</p>
+                    </Link>
+                )
+            })}
         </>
     )
 }
--- app/ui/navigationComponents/navbar.tsx
+++ app/ui/navigationComponents/navbar.tsx
@@ -1,7 +1,12 @@
+import NavLinks from "@/app/ui/navigationComponents/nav-links";
+import classes from "./navbar.module.css";
+
 export default function NavBar() {
     return (
-        <div className="navbar-container">
-
-        </div>
+        <>
+            <header className={classes.navContainer}>
+                <NavLinks />
+            </header>
+        </>
     )
 }
